86c95436fef7015abaa6afd2783defdb02239b9e
DoNalD-A <thdlstjrj@naver.com>
2024-04-21 20:44:40 +0900

    update

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name="oss_song2",
+    version="0.0.1",
+    description="oss_song's first package",
+    author="minseoksong",
+    author_email="thdlstjrj@naver.com",
+    url="https://github.com/DoNalD-A/oss_song2",
+    download_url="https://github.com/DoNalD-A/oss_song2/archive/refs/heads/main.zip",
+    install_requires=["pytest"],
+    long_description="oss-class song's first python module",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
